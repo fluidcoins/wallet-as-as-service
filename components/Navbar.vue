9f2aa8df300422386df2fd9c22f249158aa5b908
Emmahchinonso <emmanuelchinons9@gmail.com>
2021-11-02 15:06:53 +0100

    add general greeting to navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,24 +2,22 @@
   <div class="flex justify-between items-center">
     <h1 class="text-2xl">
       <span class="block font-semibold">Welcome,</span>
-      <span class="font-semibold">Moyin Olowu</span>
+      <span class="font-semibold">Developer</span>
     </h1>
     <div class="flex items-center">
       <button class="p-2 rounded-full bg-primaryLight">
         <Bell />
       </button>
-      <Avatar class="ml-12" />
+      <span class="ml-12 w-14 h-14 rounded-full bg-lightGrey" />
     </div>
   </div>
 </template>
 
 <script>
   import Bell from "~/assets/svg/bell.svg"
-  import Avatar from "~/assets/svg/avatar.svg"
   export default {
   components: {
     Bell,
-    Avatar
   },
     name: 'Navbar'
   }
